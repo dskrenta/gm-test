b3da0585ac9676f984552773d8b0e46c71313d19
dskrenta <dskrenta@gmail.com>
2016-07-17 02:51:50 -0700

    moved into files, added image overlay via composite

---
+++ overlay.js
@@ -0,0 +1,23 @@
+'use strict';
+const fs = require('fs');
+const gm = require('gm');
+
+function overlay (images, x, y) {
+  gm(`http://proxy.topixcdn.com/ipicimg/${images[0].id}-rszh${images[0].height}`)
+    .composite(`http://proxy.topixcdn.com/ipicimg/${images[1].id}-rszh${images[1].height}`)
+    .geometry(`+${x}+${y}`)
+    .write('images/overlay.jpg', (err) => {
+      if (err) console.log(err);
+    });
+}
+
+overlay([
+  {
+    id: 'MEA8SRTIVA7JE6SH',
+    height: 415
+  },
+  {
+    id: '4TEE736PM5SPCA76',
+    height: 75
+  }
+], 56, 34);
